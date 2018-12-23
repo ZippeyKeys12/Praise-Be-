d691f14f7fd037fe9492037e10bc35cc15275ee4
ZippeyKeys12 <ZippeyKeys12@gmail.com>
2018-12-23 16:56:56 -0600

    Terrestrial relations

--- src/main/java/com/zippeykeys/praisebe/deity/ModDeities.java
+++ src/main/java/com/zippeykeys/praisebe/deity/ModDeities.java
@@ -264,6 +264,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(AIR) //
                                 .alignment(NEUTRAL) //
+                                .putRelationship("morrigan", SELF) //
+                                .putRelationship("njord", INDIFFERENT) //
+                                .putRelationship("tezc", LIKED) //
+                                .putRelationship("dagda", LOVED) //
+                                .putRelationship("pan", HATED) //
+                                .putRelationship("dagon", LIKED) //
+                                .putRelationship("heph", DISLIKED) //
+                                .putRelationship("grannus", LIKED) //
+                                .putRelationship("xiuh", INDIFFERENT) //
+                                .putRelationship("poseidon", HATED) //
+                                .putRelationship("sirona", LIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 public static final Deity NJORD = Deity.builder() //
@@ -271,6 +283,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(AIR) //
                                 .alignment(NEUTRAL) //
+                                .putRelationship("morrigan", INDIFFERENT) //
+                                .putRelationship("njord", SELF) //
+                                .putRelationship("tezc", DISLIKED) //
+                                .putRelationship("dagda", DISLIKED) //
+                                .putRelationship("pan", LIKED) //
+                                .putRelationship("dagon", INDIFFERENT) //
+                                .putRelationship("heph", INDIFFERENT) //
+                                .putRelationship("grannus", DISLIKED) //
+                                .putRelationship("xiuh", LIKED) //
+                                .putRelationship("poseidon", LIKED) //
+                                .putRelationship("sirona", DISLIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 public static final Deity TEZCCATLIPOCA = Deity.builder() //
@@ -278,6 +302,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(AIR) //
                                 .alignment(NEUTRAL) //
+                                .putRelationship("morrigan", LIKED) //
+                                .putRelationship("njord", DISLIKED) //
+                                .putRelationship("tezc", SELF) //
+                                .putRelationship("dagda", LIKED) //
+                                .putRelationship("pan", LIKED) //
+                                .putRelationship("dagon", LIKED) //
+                                .putRelationship("heph", DISLIKED) //
+                                .putRelationship("grannus", INDIFFERENT) //
+                                .putRelationship("xiuh", INDIFFERENT) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", LIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 // Earth //
@@ -286,6 +322,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(EARTH) //
                                 .alignment(GOOD) //
+                                .putRelationship("morrigan", LOVED) //
+                                .putRelationship("njord", INDIFFERENT) //
+                                .putRelationship("tezc", LIKED) //
+                                .putRelationship("dagda", SELF) //
+                                .putRelationship("pan", HATED) //
+                                .putRelationship("dagon", LIKED) //
+                                .putRelationship("heph", INDIFFERENT) //
+                                .putRelationship("grannus", LIKED) //
+                                .putRelationship("xiuh", INDIFFERENT) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", LIKED) //
+                                .putRelationship("lotan", HATED) //
                                 .build(); //
 
                 public static final Deity PAN = Deity.builder() //
@@ -293,6 +341,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(EARTH) //
                                 .alignment(EVIL) //
+                                .putRelationship("morrigan", HATED) //
+                                .putRelationship("njord", LIKED) //
+                                .putRelationship("tezc", INDIFFERENT) //
+                                .putRelationship("dagda", HATED) //
+                                .putRelationship("pan", SELF) //
+                                .putRelationship("dagon", DISLIKED) //
+                                .putRelationship("heph", LIKED) //
+                                .putRelationship("grannus", INDIFFERENT) //
+                                .putRelationship("xiuh", LIKED) //
+                                .putRelationship("poseidon", LIKED) //
+                                .putRelationship("sirona", DISLIKED) //
+                                .putRelationship("lotan", LIKED) //
                                 .build(); //
 
                 public static final Deity DAGON = Deity.builder() //
@@ -300,6 +360,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(EARTH) //
                                 .alignment(GOOD) //
+                                .putRelationship("morrigan", DISLIKED) //
+                                .putRelationship("njord", LIKED) //
+                                .putRelationship("tezc", DISLIKED) //
+                                .putRelationship("dagda", LIKED) //
+                                .putRelationship("pan", DISLIKED) //
+                                .putRelationship("dagon", SELF) //
+                                .putRelationship("heph", DISLIKED) //
+                                .putRelationship("grannus", LIKED) //
+                                .putRelationship("xiuh", INDIFFERENT) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", LIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 // Fire //
@@ -308,6 +380,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(FIRE) //
                                 .alignment(NEUTRAL) //
+                                .putRelationship("morrigan", DISLIKED) //
+                                .putRelationship("njord", LIKED) //
+                                .putRelationship("tezc", INDIFFERENT) //
+                                .putRelationship("dagda", DISLIKED) //
+                                .putRelationship("pan", LIKED) //
+                                .putRelationship("dagon", DISLIKED) //
+                                .putRelationship("heph", SELF) //
+                                .putRelationship("grannus", LIKED) //
+                                .putRelationship("xiuh", INDIFFERENT) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", DISLIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 public static final Deity GRANNUS = Deity.builder() //
@@ -315,6 +399,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(FIRE) //
                                 .alignment(GOOD) //
+                                .putRelationship("morrigan", LIKED) //
+                                .putRelationship("njord", DISLIKED) //
+                                .putRelationship("tezc", INDIFFERENT) //
+                                .putRelationship("dagda", LIKED) //
+                                .putRelationship("pan", INDIFFERENT) //
+                                .putRelationship("dagon", LIKED) //
+                                .putRelationship("heph", LIKED) //
+                                .putRelationship("grannus", SELF) //
+                                .putRelationship("xiuh", INDIFFERENT) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", LIKED) //
+                                .putRelationship("lotan", HATED) //
                                 .build(); //
 
                 public static final Deity XIUHTECUHTLI = Deity.builder() //
@@ -322,6 +418,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(FIRE) //
                                 .alignment(GOOD) //
+                                .putRelationship("morrigan", INDIFFERENT) //
+                                .putRelationship("njord", LIKED) //
+                                .putRelationship("tezc", INDIFFERENT) //
+                                .putRelationship("dagda", LIKED) //
+                                .putRelationship("pan", DISLIKED) //
+                                .putRelationship("dagon", INDIFFERENT) //
+                                .putRelationship("heph", LIKED) //
+                                .putRelationship("grannus", LIKED) //
+                                .putRelationship("xiuh", SELF) //
+                                .putRelationship("poseidon", INDIFFERENT) //
+                                .putRelationship("sirona", LIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 // Water //
@@ -330,6 +438,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(WATER) //
                                 .alignment(NEUTRAL) //
+                                .putRelationship("morrigan", HATED) //
+                                .putRelationship("njord", LIKED) //
+                                .putRelationship("tezc", DISLIKED) //
+                                .putRelationship("dagda", DISLIKED) //
+                                .putRelationship("pan", LIKED) //
+                                .putRelationship("dagon", DISLIKED) //
+                                .putRelationship("heph", LIKED) //
+                                .putRelationship("grannus", DISLIKED) //
+                                .putRelationship("xiuh", DISLIKED) //
+                                .putRelationship("poseidon", SELF) //
+                                .putRelationship("sirona", DISLIKED) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 public static final Deity SIRONA = Deity.builder() //
@@ -337,6 +457,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(WATER) //
                                 .alignment(GOOD) //
+                                .putRelationship("morrigan", LIKED) //
+                                .putRelationship("njord", DISLIKED) //
+                                .putRelationship("tezc", LIKED) //
+                                .putRelationship("dagda", LIKED) //
+                                .putRelationship("pan", DISLIKED) //
+                                .putRelationship("dagon", LIKED) //
+                                .putRelationship("heph", DISLIKED) //
+                                .putRelationship("grannus", LIKED) //
+                                .putRelationship("xiuh", LIKED) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", SELF) //
+                                .putRelationship("lotan", DISLIKED) //
                                 .build(); //
 
                 public static final Deity LOTAN = Deity.builder() //
@@ -344,6 +476,18 @@ public class ModDeities {
                                 .type(TERRESTRIAL) //
                                 .element(WATER) //
                                 .alignment(EVIL) //
+                                .putRelationship("morrigan", DISLIKED) //
+                                .putRelationship("njord", DISLIKED) //
+                                .putRelationship("tezc", DISLIKED) //
+                                .putRelationship("dagda", HATED) //
+                                .putRelationship("pan", LIKED) //
+                                .putRelationship("dagon", DISLIKED) //
+                                .putRelationship("heph", DISLIKED) //
+                                .putRelationship("grannus", HATED) //
+                                .putRelationship("xiuh", DISLIKED) //
+                                .putRelationship("poseidon", DISLIKED) //
+                                .putRelationship("sirona", DISLIKED) //
+                                .putRelationship("lotan", SELF) //
                                 .build(); //
         }
 
